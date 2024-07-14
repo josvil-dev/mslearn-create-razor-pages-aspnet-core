29d577d8e633e519abe51f485a9c9d5b577e731c
Joshua Vilanculo <120592342+josvil-dev@users.noreply.github.com>
2024-07-14 10:24:53 +0000

    Added a new pizza form

--- ContosoPizza/Pages/PizzaList.cshtml.cs
+++ ContosoPizza/Pages/PizzaList.cshtml.cs
@@ -4,19 +4,39 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ContosoPizza.Pages
-{public class PizzaListModel : PageModel
 {
-    private readonly PizzaService _service;
-    public IList<Pizza> PizzaList { get;set; } = default!;
-
-    public PizzaListModel(PizzaService service)
+    public class PizzaListModel : PageModel
     {
-        _service = service;
-    }
+        [BindProperty]
+        public Pizza NewPizza { get; set; } = default!;
+        private readonly PizzaService _service;
+        public IList<Pizza> PizzaList { get; set; } = default!;
 
-    public void OnGet()
-    {
-        PizzaList = _service.GetPizzas();
+        public PizzaListModel(PizzaService service)
+        {
+            _service = service;
+        }
+
+        public void OnGet()
+        {
+            PizzaList = _service.GetPizzas();
+        }
+        public IActionResult OnPostDelete(int id)
+        {
+            _service.DeletePizza(id);
+
+            return RedirectToAction("Get");
+        }
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid || NewPizza == null)
+            {
+                return Page();
+            }
+
+            _service.AddPizza(NewPizza);
+
+            return RedirectToAction("Get");
+        }
     }
-}
 }
